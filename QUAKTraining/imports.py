1a1ef529a00adad9be399d6b1a324f2d36eb23fb
Samuel Bright-Thonney <sambt@satori-login-001.mit.edu>
2022-06-21 13:50:52 -0400

    added quak algorithm stuff

---
+++ QUAKTraining/imports.py
@@ -0,0 +1,38 @@
+import multiprocessing
+import sys
+import math
+import time
+import tqdm
+import numpy as np
+import scipy as sp
+from scipy import stats
+from scipy.spatial import ConvexHull
+import itertools
+import logging
+import matplotlib.pyplot as plt
+import pandas as pd
+import h5py
+from sklearn import metrics
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+from torch.distributions import MultivariateNormal
+import torch.utils.data as utils
+
+from argparse import ArgumentParser
+import re
+import os
+import json
+
+sys.path.append("../new_flows")
+from flows import RealNVP, Planar, MAF
+from models import NormalizingFlowModel
+from nflows.flows.base import Flow
+from nflows.flows.autoregressive import MaskedAutoregressiveFlow
+from nflows.distributions.normal import StandardNormal
+from nflows.transforms.base import CompositeTransform
+from nflows.transforms.autoregressive import MaskedAffineAutoregressiveTransform, MaskedPiecewiseQuadraticAutoregressiveTransform, MaskedPiecewiseRationalQuadraticAutoregressiveTransform
+from nflows.transforms.permutations import ReversePermutation
---
+++ QUAKTraining/samples.py
@@ -0,0 +1,23 @@
+pref_UL17 = "/nobackup/users/sambt/CASE_samples/UL_2017/"
+pref_UL18 = "/home/wmccorma/CASE_Signal_Samples_Apr14/2018/"
+bkg_loc = "/nobackup/users/sambt/CASE_samples/full_run2/newBB/BB/"
+bkg_loc_old = "/nobackup/users/sambt/CASE_samples/old_BB/"
+
+sample_library_UL17 = {"Qstar2000_W400_UL17":pref_UL17+"QstarToQW_M_2000_mW_400_TuneCP2_13TeV-pythia8_TIMBER.h5",
+                  "Wp3000_B400_UL17":pref_UL17+"WpToBpT_Wp3000_Bp400_Top170_Zbt_TuneCP5_13TeV-madgraphMLM-pythia8_TIMBER.h5",
+                  "XYY_X3000_Y80_UL17":pref_UL17+"XToYYprimeTo4Q_MX3000_MY80_MYprime170_narrow_TuneCP5_13TeV-madgraph-pythia8_TIMBER.h5"
+                  }
+
+sample_library_UL18 = {"Wp2000_Bp25_UL18":pref_UL18+"WpToBpT_Wp2000_Bp25_Top170_Zbt_TuneCP5_13TeV-madgraphMLM-pythia8_TIMBER.h5",
+                       "Wp3000_Bp170_UL18":pref_UL18+"WpToBpT_Wp3000_Bp170_Top170_Zbt_TuneCP5_13TeV-madgraphMLM-pythia8_TIMBER.h5",
+                       "Wp5000_Bp400_UL18":pref_UL18+"WpToBpT_Wp5000_Bp400_Top170_Zbt_TuneCP5_13TeV-madgraphMLM-pythia8_TIMBER.h5",
+                       "RSG2000_UL18":pref_UL18+"RSGravitonToGluonGluon_kMpl01_M_2000_TuneCP5_13TeV_pythia8_TIMBER.h5",
+                       "RSG3000_UL18":pref_UL18+"RSGravitonToGluonGluon_kMpl01_M_3000_TuneCP5_13TeV_pythia8_TIMBER.h5",
+                       "RSG5000_UL18":pref_UL18+"RSGravitonToGluonGluon_kMpl01_M_5000_TuneCP5_13TeV_pythia8_TIMBER.h5",
+                       "Qstar2000_W25_UL18":pref_UL18+"QstarToQW_M_2000_mW_25_TuneCP2_13TeV-pythia8_TIMBER.h5",
+                       "Qstar3000_W170_UL18":pref_UL18+"QstarToQW_M_3000_mW_170_TuneCP2_13TeV-pythia8_TIMBER.h5",
+                       "Qstar5000_W400_UL18":pref_UL18+"QstarToQW_M_5000_mW_400_TuneCP2_13TeV-pythia8_TIMBER.h5",
+                       "Wkk2000_r170_UL18":pref_UL18+"WkkToWRadionToWWW_M2000_Mr170_TuneCP5_13TeV-madgraph-pythia8_TIMBER.h5",
+                       "Wkk3000_r170_UL18":pref_UL18+"WkkToWRadionToWWW_M3000_Mr170_TuneCP5_13TeV-madgraph-pythia8_TIMBER.h5",
+                       "Wkk5000_r400_UL18":pref_UL18+"WkkToWRadionToWWW_M5000_Mr400_TuneCP5_13TeV-madgraph-pythia8_TIMBER.h5"
+                       }
